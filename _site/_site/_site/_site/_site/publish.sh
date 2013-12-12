8bf37739527283fd4da8a0145db73619866d710e
Elricstorm <jdezenzio@gmail.com>
2013-12-12 15:35:57 -0500

    update website

---
+++ _site/_site/_site/_site/_site/publish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "publishing to the rails-windows.github.io site"
+cd ~/workspace/rails-windows/rails-windows.github.io.wiki
+gollum-site generate
+echo "GENERATED";
+cp -R _site/* ../rails-windows.github.io
+rm -rf _site/
+cd ../rails-windows.github.io
+mv Home.html index.html
+echo "COMMITTING";
+git add -A
+git commit -am "update website"
+git push origin master
+echo "FINISHED";
+exit 0
